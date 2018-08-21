145e814be020d3dbeb5b7310ccafdf457e9ea380
eswar2106 <eswarmavuru1607@gmail.com>
2018-08-21 12:10:14 +0530

    first commit of python programs

---
+++ miniongame.py
@@ -0,0 +1,19 @@
+def minion_game(string):
+    # your code goes here
+    ke=0
+    st=0
+    vowels='AEIOU'
+    n=len(string)
+    for i in range(n):
+        if string[i] in vowels:
+            ke=ke+(n-i)
+        else:
+            st=st+(n-i)
+    if ke>st:
+        print("Kevin",ke)
+    elif st>ke:
+        print("Stuart",st)
+    else:
+        print("Draw")
+
+
